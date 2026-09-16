97771038521e003d4b8bf36b3f9871dfc6262db7
Данила П <danilapuzanov48@gmail.com>
2026-09-16 18:11:18 +0300

    Добавлено объявление класса MathTest + создан файл MathTest.cpp

--- Lib/MathTest.h
+++ Lib/MathTest.h
@@ -1,3 +1,19 @@
 #pragma once
+#include "Task.h"
 
-class MathTest;
+class MathTest {
+	Task* _tasks;
+	int _count_of_tasks;
+	double* _user_answers;
+	int _count_of_correct_answers;
+
+public:
+	MathTest(int count_of_tasks);
+	MathTest(int count_of_tasks, int min, int max);
+	MathTest(int count_of_tasks, int min, int max, char operation);
+
+	~MathTest();
+
+	void run();
+	void show_statistics() const noexcept;
+};
